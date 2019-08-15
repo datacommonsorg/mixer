995d887b314f0847fbfd321297ea526c8067068b
William Apollo <4895865+Spaceenter@users.noreply.github.com>
2019-08-14 18:04:55 -0700

    Add more precondition checks for DCIDs in requests. (#35)

--- util/util.go
+++ util/util.go
@@ -315,3 +315,13 @@ func BigTableReadRowsParallel(ctx context.Context, btTable *bigtable.Table,
 
 	return nil
 }
+
+// CheckValidDCIDs checks if DCIDs are valid. More criteria will be added as being discovered.
+func CheckValidDCIDs(dcids []string) bool {
+	for _, dcid := range dcids {
+		if strings.Contains(dcid, " ") || strings.Contains(dcid, ",") {
+			return false
+		}
+	}
+	return true
+}
