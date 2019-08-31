26aec1c264eccea5d9d8ba05f46bda138cd272da
Bo Xu <boxu@google.com>
2019-08-30 19:17:15 -0700

    Use sparql in Translate api and add rest endpoint

--- examples/query/main.go
+++ examples/query/main.go
@@ -40,7 +40,7 @@ func main() {
 
 	ctx := context.Background()
 
-	// Contact the server and print out its response.
+	// Send a bunch of Sparql query and print out its response.
 	qStrs := []string{
 		`
 		BASE <http://schema.org/>
@@ -84,4 +84,54 @@ func main() {
 		}
 		log.Printf("Query: %v", r.GetRows())
 	}
+
+	// Send a transate request and print the response.
+	sm := `
+	Node: E:StatisticalPopulation->E1
+	typeOf: StatisticalPopulation
+	dcid: C:StatisticalPopulation->Col.row_info.id
+	location: E:StatisticalPopulation->E2
+	functionalDeps: dcid
+
+	Node: E:StatisticalPopulation->E2
+	typeOf: Place
+	dcid: C:StatisticalPopulation->Col.place_key
+	functionalDeps: dcid
+
+	Node: E:Place->E1
+	typeOf: Place
+	subType: C:Place->Col.type
+	dcid: C:Place->Col.row_info.id
+	name: C:Place->Col.name
+	functionalDeps: dcid
+
+	Node: E:Triple->E1
+	dcid: C:Triple->Col.subject_id
+	provenance: E:Triple->E2
+	C:Triple->Col.predicate: C:Triple->Col.object_value
+	functionalDeps: dcid
+
+	Node: E:Triple->E2
+	typeOf: Provenance
+	dcid: C:Triple->Col.row_info.prov_id
+	functionalDeps: dcid
+	`
+	q := `
+	SELECT ?dcid
+	WHERE {
+		?pop typeOf StatisticalPopulation .
+		?place typeOf State .
+		?pop location ?place .
+		?pop dcid ?dcid
+		?place name "California" .
+	}
+	`
+	r, err := c.Translate(ctx, &pb.TranslateRequest{
+		Sparql:        q,
+		SchemaMapping: sm,
+	})
+	if err != nil {
+		log.Fatalf("could not Transate: %v", err)
+	}
+	log.Printf("Query: %v", r.GetTranslation())
 }
