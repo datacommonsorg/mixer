3d7e3c98db19c7617ba1d20054227eeca2cf9608
Bo Xu <boxu@google.com>
2020-04-02 10:04:31 -0700

    Update bigtable test support and start to add test for get_populations

--- store/core_test.go
+++ store/core_test.go
@@ -3,12 +3,12 @@ package store
 import (
 	"context"
 	"encoding/json"
-	"reflect"
 	"testing"
 
 	pb "github.com/datacommonsorg/mixer/proto"
 	"github.com/datacommonsorg/mixer/util"
-	"google.golang.org/api/option"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
 )
 
 func TestGetPropertyLabels(t *testing.T) {
@@ -36,11 +36,11 @@ func TestGetPropertyLabels(t *testing.T) {
 	} {
 		jsonRaw, err := json.Marshal(d.labels)
 		if err != nil {
-			t.Fatalf("json.Marshal(%v) = %v", d.dcid, err)
+			t.Errorf("json.Marshal(%+v) = %+v", d.dcid, err)
 		}
 		tableValue, err := util.ZipAndEncode(string(jsonRaw))
 		if err != nil {
-			t.Fatalf("util.ZipAndEncode(%v) = %v", d.dcid, err)
+			t.Errorf("util.ZipAndEncode(%+v) = %+v", d.dcid, err)
 		}
 		data[util.BtArcsPrefix+d.dcid] = tableValue
 
@@ -55,20 +55,15 @@ func TestGetPropertyLabels(t *testing.T) {
 
 	wantPayloadRaw, err := json.Marshal(resultMap)
 	if err != nil {
-		t.Fatalf("json.Marshal(%v) = %v", resultMap, err)
+		t.Fatalf("json.Marshal(%+v) = %+v", resultMap, err)
 	}
 	want := &pb.GetPropertyLabelsResponse{
 		Payload: string(wantPayloadRaw),
 	}
 
-	conn, err := setupBigtable(ctx, data)
+	store, err := NewTestBtStore(ctx, data)
 	if err != nil {
-		t.Fatalf("setupBigTable() = %v", err)
-	}
-
-	store, err := newTestBtStore(ctx, option.WithGRPCConn(conn))
-	if err != nil {
-		t.Fatalf("newTestBtStore() = %v", err)
+		t.Fatalf("NewTestBtStore() = %+v", err)
 	}
 
 	var out pb.GetPropertyLabelsResponse
@@ -76,10 +71,10 @@ func TestGetPropertyLabels(t *testing.T) {
 		&pb.GetPropertyLabelsRequest{
 			Dcids: []string{"geoId/06", "bio/tiger"},
 		}, &out); err != nil {
-		t.Fatalf("GetPropertyLabels() = %v", err)
+		t.Fatalf("GetPropertyLabels() = %+v", err)
 	}
 
-	if got := &out; !reflect.DeepEqual(got, want) {
-		t.Errorf("GetPropertyLabels() = %v, want %v", got, want)
+	if got := &out; !cmp.Equal(got, want, protocmp.Transform()) {
+		t.Errorf("GetPropertyLabels() = %+v, want %+v", got, want)
 	}
 }
