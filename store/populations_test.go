8ca754cb7c25472e617547babf214c8e6a600b69
Bo Xu <shifucun@users.noreply.github.com>
2020-04-02 16:38:12 -0700

    Add unitest for get_popobs cache merging (#134)

--- store/populations_test.go
+++ store/populations_test.go
@@ -20,6 +20,9 @@ import (
 
 	pb "github.com/datacommonsorg/mixer/proto"
 	"github.com/datacommonsorg/mixer/util"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
 )
 
 func TestGetPopObs(t *testing.T) {
@@ -27,50 +30,50 @@ func TestGetPopObs(t *testing.T) {
 	dcid := "geoId/06"
 	key := util.BtPopObsPrefix + dcid
 	btRow := `{
-	"name": "Santa Clara",
-	"populations": {
-		"dc/p/zzlmxxtp1el87": {
-		"popType": "Household",
-		"numConstraints": 3,
-		"propertyValues": {
-			"householderAge": "Years45To64",
-			"householderRace": "AsianAlone",
-			"income": "USDollar35000To39999"
+		"name": "Santa Clara",
+		"populations": {
+			"dc/p/zzlmxxtp1el87": {
+				"popType": "Household",
+				"numConstraints": 3,
+				"propertyValues": {
+					"householderAge": "Years45To64",
+					"householderRace": "AsianAlone",
+					"income": "USDollar35000To39999"
+				},
+				"observations": [
+					{
+						"marginOfError": 274,
+						"measuredProp": "count",
+						"measuredValue": 1352,
+						"measurementMethod": "CensusACS5yrSurvey",
+						"observationDate": "2017"
+					},
+					{
+						"marginOfError": 226,
+						"measuredProp": "count",
+						"measuredValue": 1388,
+						"measurementMethod": "CensusACS5yrSurvey",
+						"observationDate": "2013"
+					}
+				]
+			}
 		},
 		"observations": [
 			{
-			"marginOfError": 274,
-			"measuredProp": "count",
-			"measuredValue": 1352,
-			"measurementMethod": "CensusACS5yrSurvey",
-			"observationDate": "2017"
+				"meanValue": 4.1583,
+				"measuredProp": "particulateMatter25",
+				"measurementMethod": "CDCHealthTracking",
+				"observationDate": "2014-04-04",
+				"observedNode": "geoId/06085"
 			},
 			{
-			"marginOfError": 226,
-			"measuredProp": "count",
-			"measuredValue": 1388,
-			"measurementMethod": "CensusACS5yrSurvey",
-			"observationDate": "2013"
+				"meanValue": 9.4461,
+				"measuredProp": "particulateMatter25",
+				"measurementMethod": "CDCHealthTracking",
+				"observationDate": "2014-03-20",
+				"observedNode": "geoId/06085"
 			}
-		],
-		},
-	},
-	"observations": [
-		{
-		"meanValue": 4.1583,
-		"measuredProp": "particulateMatter25",
-		"measurementMethod": "CDCHealthTracking",
-		"observationDate": "2014-04-04",
-		"observedNode": "geoId/06085"
-		},
-		{
-		"meanValue": 9.4461,
-		"measuredProp": "particulateMatter25",
-		"measurementMethod": "CDCHealthTracking",
-		"observationDate": "2014-03-20",
-		"observedNode": "geoId/06085"
-		}
-	]
+		]
 	}`
 
 	tableValue, err := util.ZipAndEncode(string(btRow))
@@ -92,5 +95,101 @@ func TestGetPopObs(t *testing.T) {
 	s.GetPopObs(context.Background(), in, &out)
 	if out.GetPayload() != tableValue {
 		t.Errorf("GetPopObs() = %+v; want: %v", out.GetPayload(), tableValue)
+	}
+}
+
+func TestGetPopObsCacheMerge(t *testing.T) {
+	dcid := "geoId/06"
+	key := util.BtPopObsPrefix + dcid
+
+	// Setup bigtable
+	baseData := map[string]string{}
+	btRow := `{
+		"name": "Santa Clara",
+		"populations": {
+			"dc/p/abcxyz": {
+				"popType": "Household",
+				"numConstraints": 3,
+				"propertyValues": {
+					"householderAge": "Years45To64",
+					"householderRace": "AsianAlone",
+					"income": "USDollar35000To39999"
+				},
+				"observations": [
+					{
+						"marginOfError": 226,
+						"measuredProp": "count",
+						"measuredValue": 1388,
+						"measurementMethod": "CensusACS5yrSurvey",
+						"observationDate": "2013"
+					}
+				]
+			}
+		}
+	}`
+	tableValue, err := util.ZipAndEncode(btRow)
+	if err != nil {
+		t.Errorf("util.ZipAndEncode(%+v) = %v", btRow, err)
+	}
+	baseData[key] = tableValue
+	btClient, err := SetupBigtable(context.Background(), baseData)
+	if err != nil {
+		t.Errorf("SetupBigtable(...) = %v", err)
+	}
+
+	// branch cache data. Have observation on newer date.
+	branchData := map[string]string{}
+	branchCache := `{
+		"name": "Santa Clara",
+		"populations": {
+			"dc/p/abcxyz": {
+				"popType": "Household",
+				"numConstraints": 3,
+				"propertyValues": {
+					"householderAge": "Years45To64",
+					"householderRace": "AsianAlone",
+					"income": "USDollar35000To39999"
+				},
+				"observations": [
+					{
+						"marginOfError": 274,
+						"measuredProp": "count",
+						"measuredValue": 1352,
+						"measurementMethod": "CensusACS5yrSurvey",
+						"observationDate": "2017"
+					},
+					{
+						"marginOfError": 226,
+						"measuredProp": "count",
+						"measuredValue": 1388,
+						"measurementMethod": "CensusACS5yrSurvey",
+						"observationDate": "2013"
+					}
+				]
+			}
+		}
+	}`
+	branchCacheValue, err := util.ZipAndEncode(branchCache)
+	if err != nil {
+		t.Errorf("util.ZipAndEncode(%+v) = %v", branchCache, err)
+	}
+	branchData[key] = branchCacheValue
+	// Test
+	s, err := &store{
+		"", nil, nil, nil, nil, nil, nil, btClient.Open("dc"), branchData}, nil
+	in := &pb.GetPopObsRequest{
+		Dcid: dcid,
+	}
+	var out pb.GetPopObsResponse
+	s.GetPopObs(context.Background(), in, &out)
+
+	var resultProto, expectProto pb.PopObsPlace
+	if tmp, err := util.UnzipAndDecode(out.GetPayload()); err == nil {
+		jsonpb.UnmarshalString(string(tmp), &resultProto)
+	}
+	jsonpb.UnmarshalString(branchCache, &expectProto)
+
+	if diff := cmp.Diff(resultProto, expectProto, protocmp.Transform()); diff != "" {
+		t.Errorf("GetPopObs() got diff %+v", diff)
 	}
 }
