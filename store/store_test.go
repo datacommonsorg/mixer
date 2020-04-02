e9c0974672e6834adba7cd60bfd9c0e7e67afd43
Bo Xu <shifucun@users.noreply.github.com>
2020-04-02 11:54:22 -0700

    Update bigtable test support and start to add test for get_populations (#133)
    * Update bigtable test support and start to add test for get_populations
    
    * update

--- store/store_test.go
+++ store/store_test.go
@@ -2,10 +2,13 @@ package store
 
 import (
 	"context"
+	"testing"
 
 	"cloud.google.com/go/bigtable"
 	"cloud.google.com/go/bigtable/bttest"
+	pb "github.com/datacommonsorg/mixer/proto"
 	"github.com/datacommonsorg/mixer/util"
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 )
@@ -16,17 +19,16 @@ const (
 	testTable    = "dc"
 )
 
-func newTestBtStore(ctx context.Context, opts ...option.ClientOption) (Interface, error) {
-	btClient, err := bigtable.NewClient(ctx, testProject, testInstance, opts...)
+// NewTestBtStore creates a new store for testing BigTable.
+func NewTestBtStore(ctx context.Context, data map[string]string) (Interface, error) {
+	btClient, err := SetupBigtable(ctx, data)
 	if err != nil {
 		return nil, err
 	}
-
-	return &store{
-		"", nil, nil, nil, nil, nil, nil, btClient.Open(testTable), nil}, nil
+	return &store{"", nil, nil, nil, nil, nil, nil, btClient.Open(testTable), nil}, nil
 }
 
-func setupBigtable(ctx context.Context, data map[string]string) (*grpc.ClientConn, error) {
+func SetupBigtable(ctx context.Context, data map[string]string) (*bigtable.Client, error) {
 	srv, err := bttest.NewServer("localhost:0")
 	if err != nil {
 		return nil, err
@@ -65,6 +67,54 @@ func setupBigtable(ctx context.Context, data map[string]string) (*grpc.ClientCon
 			return nil, err
 		}
 	}
+	return client, err
+}
 
-	return conn, err
+func TestReadRowsParallel(t *testing.T) {
+	ctx := context.Background()
+	data := map[string]string{
+		"key1": "data1",
+		"key2": "data2",
+	}
+	btClient, err := SetupBigtable(ctx, data)
+	if err != nil {
+		t.Errorf("setupBigtable got error: %v", err)
+	}
+	btTable := btClient.Open(testTable)
+	results := map[string]string{}
+	rowList := bigtable.RowList{"key1", "key2"}
+	if err := bigTableReadRowsParallel(ctx, btTable, rowList,
+		func(btRow bigtable.Row) error {
+			results[btRow.Key()] = string(btRow[util.BtFamily][0].Value)
+			return nil
+		}); err != nil {
+		t.Errorf("btReadRowsParallel got error: %v", err)
+	}
+	if diff := cmp.Diff(data, results); diff != "" {
+		t.Errorf("read rows got diff from table data %+v", diff)
+	}
+}
+
+func TestIsterateSortPVs(t *testing.T) {
+	var pvs = []*pb.PropertyValue{
+		&pb.PropertyValue{
+			Property: "gender",
+			Value:    "Male",
+		},
+		&pb.PropertyValue{
+			Property: "age",
+			Value:    "Years85Onwards",
+		},
+	}
+	got := "^populationType"
+	if len(pvs) > 0 {
+		iterateSortPVs(pvs, func(i int, p, v string) {
+			got += ("^" + p + "^" + v)
+		})
+	}
+
+	want := "^populationType^age^Years85Onwards^gender^Male"
+	if got != want {
+		t.Errorf("iterateSortPVs() = %s, want %s", got, want)
+	}
 }
