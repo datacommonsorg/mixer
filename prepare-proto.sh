347a6c3528f20a44adda0b19cd89bf397af76b0c
Bo Xu <boxu@google.com>
2020-07-11 14:42:24 -0700

    Use prepare-proto.sh to wrap proto preparation

---
+++ prepare-proto.sh
@@ -0,0 +1,23 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+go get google.golang.org/protobuf/cmd/protoc-gen-go
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
+mkdir -p proto/google/api/
+curl -sSL https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto \
+      --output proto/google/api/annotations.proto
+curl -sSL https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto \
+      --output proto/google/api/http.proto
