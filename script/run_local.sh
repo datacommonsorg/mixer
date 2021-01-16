b557e81b374dd06a682e062bd3b23c07dbf1b294
Bo Xu <boxu@google.com>
2021-01-15 21:05:23 -0800

    update

--- script/run_local.sh
+++ script/run_local.sh
@@ -18,8 +18,7 @@ set -e
 
 # Sparql Query
 curl localhost:9090/query?sparql=SELECT%20%3Fname%20WHERE%20%7B%20%3Fstate%20typeOf%20State%20.%20%3Fstate%20dcid%20geoId%2F06%20.%20%3Fstate%20name%20%3Fname%20%7D
-echo ""
-
+echo
 # Property Labels
 curl localhost:9090/node/property-labels?dcids=geoId/05&dcids=geoId/06
-echo ""
+echo
