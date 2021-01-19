57bf319fb50328a5364783796399d55196b1055d
Bo Xu <boxu@google.com>
2021-01-19 11:09:44 -0800

    comment

--- script/run_local.sh
+++ script/run_local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright 2019 Google LLC
+#
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# Script to make test requests to local mixer.
 
 set -e
 
