dabc81fb76da7a6bb01bcfa6e862dbdeaea324bc
Bo Xu <boxu@google.com>
2021-07-01 12:54:29 -0700

    Return union of all valid stat vars for all places

--- internal/server/statvar_search.go
+++ internal/server/statvar_search.go
@@ -87,7 +87,7 @@ func filter(
 	numPlaces int) []*pb.EntityInfo {
 	result := []*pb.EntityInfo{}
 	for _, node := range nodes {
-		if existence, ok := countMap[node.Dcid]; ok && len(existence) == numPlaces {
+		if existence, ok := countMap[node.Dcid]; ok && len(existence) > 0 {
 			result = append(result, node)
 		}
 	}
