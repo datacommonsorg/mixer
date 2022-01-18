a6274eaae09e5687d7c6d03c4050ad0768f195a4
Anush Kini <anushk@google.com>
2022-01-18 09:18:16 +0000

    Added FBI Hate Crime Ranking

--- internal/server/ranking/ranking.go
+++ internal/server/ranking/ranking.go
@@ -68,6 +68,10 @@ var StatsRanking = map[RankKey]int{
 	{"NASA_WetBulbComputation_Aggregation", "NASA_Mean_HadGEM2-AO", "*"}: 0, // Wet bulb year aggregation
 	{"NASA_WetBulbComputation_Aggregation", "*", "*"}:                    1, // Wet bulb year aggregation
 	{"NASA_WetBulbComputation", "NASA_Mean_HadGEM2-AO", "*"}:             2, // Wet bulb
+
+	// Prefer FBI Hate Crime Publications over Data Commons Aggregate
+	{"FBIHateCrimePublications", "*", "*"}:        0, // FBI Hate Crime Publications
+	{"FBIHateCrime", "DataCommonsAggregate", "*"}: 1, // FBI Hate Crime Aggregations
 }
 
 // BaseRank is the base ranking score for sources. If a source is prefered, it
