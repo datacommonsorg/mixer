8b08459319b69a70f2b48e73f4809ed8bb11b877
William Apollo <4895865+Spaceenter@users.noreply.github.com>
2022-02-03 12:10:49 -0800

    update golden tests for import group for get_stat{_all, _series, s} (#722)

--- internal/server/stat/golden/get_stat_series_test.go
+++ internal/server/stat/golden/get_stat_series_test.go
@@ -30,85 +30,93 @@ func TestGetStatSeries(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 
-	client, _, err := e2e.Setup()
-	if err != nil {
-		t.Fatalf("Failed to set up mixer and client")
-	}
-	_, filename, _, _ := runtime.Caller(0)
-	goldenPath := path.Join(
-		path.Dir(filename), "get_stat_series")
-
-	for _, c := range []struct {
-		statVar    string
-		place      string
-		goldenFile string
-		mmethod    string
-	}{
-		{
-			"Count_Person",
-			"country/USA",
-			"count_person.json",
-			"CensusACS5yrSurvey",
-		},
-		{
-			"Count_CriminalActivities_CombinedCrime",
-			"geoId/06",
-			"total_crimes.json",
-			"",
-		},
-		{
-			"Annual_Generation_Electricity",
-			"geoId/06",
-			"electricity_generation.json",
-			"",
-		},
-		{
-			"Median_Age_Person",
-			"geoId/0649670",
-			"median_age.json",
-			"",
-		},
-		{
-			"Amount_EconomicActivity_GrossNationalIncome_PurchasingPowerParity_PerCapita",
-			"country/USA",
-			"gdp.json",
-			"",
-		},
-		{
-			"dummy_sv",
-			"geoId/06",
-			"dummy_sv.json",
-			"",
-		},
-		{
-			"Count_Person",
-			"dummy_place",
-			"dummy_place.json",
-			"",
-		},
+	for _, opt := range []*e2e.TestOption{
+		{},
+		{UseImportGroup: true},
 	} {
-		resp, err := client.GetStatSeries(ctx, &pb.GetStatSeriesRequest{
-			StatVar:           c.statVar,
-			Place:             c.place,
-			MeasurementMethod: c.mmethod,
-		})
+		client, _, err := e2e.Setup(opt)
 		if err != nil {
-			t.Errorf("could not GetStatSeries: %s", err)
-			continue
+			t.Fatalf("Failed to set up mixer and client")
 		}
-		if e2e.GenerateGolden {
-			e2e.UpdateGolden(resp, goldenPath, c.goldenFile)
-			continue
-		}
-		var expected pb.GetStatSeriesResponse
-		if err = e2e.ReadJSON(goldenPath, c.goldenFile, &expected); err != nil {
-			t.Errorf("Can not Unmarshal golden file: %s", err)
-			continue
-		}
+		_, filename, _, _ := runtime.Caller(0)
+		goldenPath := path.Join(
+			path.Dir(filename), "get_stat_series")
 
-		if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
-			t.Errorf("payload got diff: %v", diff)
-			continue
+		for _, c := range []struct {
+			statVar    string
+			place      string
+			goldenFile string
+			mmethod    string
+		}{
+			{
+				"Count_Person",
+				"country/USA",
+				"count_person.json",
+				"CensusACS5yrSurvey",
+			},
+			{
+				"Count_CriminalActivities_CombinedCrime",
+				"geoId/06",
+				"total_crimes.json",
+				"",
+			},
+			{
+				"Annual_Generation_Electricity",
+				"geoId/06",
+				"electricity_generation.json",
+				"",
+			},
+			{
+				"Median_Age_Person",
+				"geoId/0649670",
+				"median_age.json",
+				"",
+			},
+			{
+				"Amount_EconomicActivity_GrossNationalIncome_PurchasingPowerParity_PerCapita",
+				"country/USA",
+				"gdp.json",
+				"",
+			},
+			{
+				"dummy_sv",
+				"geoId/06",
+				"dummy_sv.json",
+				"",
+			},
+			{
+				"Count_Person",
+				"dummy_place",
+				"dummy_place.json",
+				"",
+			},
+		} {
+			if opt.UseImportGroup {
+				c.goldenFile = "IG_" + c.goldenFile
+			}
+			resp, err := client.GetStatSeries(ctx, &pb.GetStatSeriesRequest{
+				StatVar:           c.statVar,
+				Place:             c.place,
+				MeasurementMethod: c.mmethod,
+			})
+			if err != nil {
+				t.Errorf("could not GetStatSeries: %s", err)
+				continue
+			}
+			if e2e.GenerateGolden {
+				e2e.UpdateGolden(resp, goldenPath, c.goldenFile)
+				continue
+			}
+			var expected pb.GetStatSeriesResponse
+			if err = e2e.ReadJSON(goldenPath, c.goldenFile, &expected); err != nil {
+				t.Errorf("Can not Unmarshal golden file: %s", err)
+				continue
+			}
+
+			if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
+				t.Errorf("payload got diff: %v", diff)
+				continue
+			}
 		}
 	}
 }
