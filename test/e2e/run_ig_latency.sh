dff77a168c6299a039ae9f7fc5e49a3cbcd5c8b4
Wei Sun <wsws@google.com>
2022-02-28 13:22:17 -0800

    resolve codacy.com issues

--- test/e2e/run_ig_latency.sh
+++ test/e2e/run_ig_latency.sh
@@ -20,6 +20,6 @@ go test -parallel 1 ./...
 
 cd test/e2e/ig_latency
 echo "method,no-import-group,import-group,change-rate" > ig_latency
-cat *.csv >> ig_latency
-rm *.csv
+cat ./*.csv >> ig_latency
+rm ./*.csv
 mv ig_latency ig_latency.csv
