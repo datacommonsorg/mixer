51585b3e5cd4f50ea5c93040e08d8cc531729e53
Bo Xu <shifucun@users.noreply.github.com>
2022-04-28 16:46:44 -0700

    Add protobuf for pagination and functions to encode/decode pagination proto (#837)
    * Add protobuf for pagination and functions to encode/decode pagination proto
    
    * lint
    
    * naming

---
+++ internal/server/pagination/pagination_test.go
@@ -0,0 +1,145 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pagination
+
+import (
+	"testing"
+
+	pb "github.com/datacommonsorg/mixer/internal/proto"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestDencode(t *testing.T) {
+	for _, c := range []struct {
+		info  *pb.PaginationInfo
+		token string
+	}{
+		{
+			// One entity scenario.
+			&pb.PaginationInfo{
+				CursorGroups: []*pb.ImportGroupCursorGroup{
+					{
+						Cursors: []*pb.ImportGroupCursor{
+							{
+								ImportGroup: 0,
+								Page:        0,
+								Item:        20,
+							},
+							{
+								ImportGroup: 1,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 2,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 3,
+								Page:        2,
+								Item:        50,
+							},
+							{
+								ImportGroup: 4,
+								Page:        1,
+								Item:        10,
+							},
+						},
+					},
+				},
+			},
+			"H4sIAAAAAAAA/+JSEWKSEBFi42AUYJTgEmLjYILSzAJMEkZCbBwsID4AAAD//wEAAP//htWoVyYAAAA=",
+		},
+		{
+			// Multiple entity scenario.
+			&pb.PaginationInfo{
+				CursorGroups: []*pb.ImportGroupCursorGroup{
+					{
+						Key: "geoId/05",
+						Cursors: []*pb.ImportGroupCursor{
+							{
+								ImportGroup: 0,
+								Page:        0,
+								Item:        20,
+							},
+							{
+								ImportGroup: 1,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 2,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 3,
+								Page:        2,
+								Item:        50,
+							},
+							{
+								ImportGroup: 4,
+								Page:        1,
+								Item:        10,
+							},
+						},
+					},
+					{
+						Key: "geoId/06",
+						Cursors: []*pb.ImportGroupCursor{
+							{
+								ImportGroup: 0,
+								Page:        5,
+								Item:        200,
+							},
+							{
+								ImportGroup: 1,
+								Page:        8,
+								Item:        100,
+							},
+							{
+								ImportGroup: 2,
+								Page:        7,
+								Item:        150,
+							},
+							{
+								ImportGroup: 3,
+								Page:        15,
+								Item:        60,
+							},
+							{
+								ImportGroup: 4,
+								Page:        4,
+								Item:        40,
+							},
+						},
+					},
+				},
+			},
+			"H4sIAAAAAAAA/+LS4+JIT833TNE3MBVikhARYuNgFGCU4BJi42CC0swCTBJGQmwcLCA+lxFcvZkQqwCrxAlGsBYOiRQhdg4mAXaJaYxgPfwSNmA9LBIaAAAAAP//AQAA//82b3t4ZAAAAA==",
+		},
+	} {
+		info, err := Decode(c.token)
+		if err != nil {
+			t.Errorf("Decode() got err %v", err)
+			continue
+		}
+		if diff := cmp.Diff(info, c.info, protocmp.Transform()); diff != "" {
+			t.Errorf("getScorePb() got diff score %v", diff)
+		}
+	}
+}
--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package util
 
 import (
@@ -224,6 +238,128 @@ func TestParseBigtableGroup(t *testing.T) {
 		got := ParseBigtableGroup(c.input)
 		if diff := cmp.Diff(got, c.want); diff != "" {
 			t.Errorf("ParseBigtableGroup got diff %+v", diff)
+		}
+	}
+}
+
+func TestEncode(t *testing.T) {
+	for _, c := range []struct {
+		info  *pb.PaginationInfo
+		token string
+	}{
+		{
+			// One entity scenario.
+			&pb.PaginationInfo{
+				CursorGroups: []*pb.ImportGroupCursorGroup{
+					{
+						Cursors: []*pb.ImportGroupCursor{
+							{
+								ImportGroup: 0,
+								Page:        0,
+								Item:        20,
+							},
+							{
+								ImportGroup: 1,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 2,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 3,
+								Page:        2,
+								Item:        50,
+							},
+							{
+								ImportGroup: 4,
+								Page:        1,
+								Item:        10,
+							},
+						},
+					},
+				},
+			},
+			"H4sIAAAAAAAA/+JSEWKSEBFi42AUYJTgEmLjYILSzAJMEkZCbBwsID4AAAD//wEAAP//htWoVyYAAAA=",
+		},
+		{
+			// Multiple entity scenario.
+			&pb.PaginationInfo{
+				CursorGroups: []*pb.ImportGroupCursorGroup{
+					{
+						Key: "geoId/05",
+						Cursors: []*pb.ImportGroupCursor{
+							{
+								ImportGroup: 0,
+								Page:        0,
+								Item:        20,
+							},
+							{
+								ImportGroup: 1,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 2,
+								Page:        1,
+								Item:        10,
+							},
+							{
+								ImportGroup: 3,
+								Page:        2,
+								Item:        50,
+							},
+							{
+								ImportGroup: 4,
+								Page:        1,
+								Item:        10,
+							},
+						},
+					},
+					{
+						Key: "geoId/06",
+						Cursors: []*pb.ImportGroupCursor{
+							{
+								ImportGroup: 0,
+								Page:        5,
+								Item:        200,
+							},
+							{
+								ImportGroup: 1,
+								Page:        8,
+								Item:        100,
+							},
+							{
+								ImportGroup: 2,
+								Page:        7,
+								Item:        150,
+							},
+							{
+								ImportGroup: 3,
+								Page:        15,
+								Item:        60,
+							},
+							{
+								ImportGroup: 4,
+								Page:        4,
+								Item:        40,
+							},
+						},
+					},
+				},
+			},
+			"H4sIAAAAAAAA/+LS4+JIT833TNE3MBVikhARYuNgFGCU4BJi42CC0swCTBJGQmwcLCA+lxFcvZkQqwCrxAlGsBYOiRQhdg4mAXaJaYxgPfwSNmA9LBIaAAAAAP//AQAA//82b3t4ZAAAAA==",
+		},
+	} {
+		token, err := EncodeProto(c.info)
+		if err != nil {
+			t.Errorf("EncodeProto() got err %v", err)
+			continue
+		}
+		if diff := cmp.Diff(token, c.token); diff != "" {
+			t.Errorf("getScorePb() got diff score %v", diff)
 		}
 	}
 }
