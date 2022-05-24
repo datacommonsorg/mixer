b08bd30603b78e7da95616f89844d43bf927a97e
Bo Xu <shifucun@users.noreply.github.com>
2022-05-24 07:58:41 -0700

    [V1 API] Implement paginated bulk triples API (#852)
    * [V1 API] Implement paginated bulk triples API
    
    * change data to triples
    
    * fix tests

--- internal/server/v1/triples/triples.go
+++ internal/server/v1/triples/triples.go
@@ -44,9 +44,6 @@ func Triples(
 		return nil, status.Errorf(
 			codes.InvalidArgument, "invalid entity %s", entity)
 	}
-	res := &pb.TriplesResponse{
-		Data: map[string]*pb.EntityInfoCollection{},
-	}
 	propsResp, err := properties.Properties(
 		ctx, &pb.PropertiesRequest{
 			Entity:    entity,
@@ -70,11 +67,102 @@ func Triples(
 	if err != nil {
 		return nil, err
 	}
+	res := &pb.TriplesResponse{
+		Triples: map[string]*pb.EntityInfoCollection{},
+	}
 	for property := range data {
-		res.Data[property] = &pb.EntityInfoCollection{
+		res.Triples[property] = &pb.EntityInfoCollection{
 			Entities: data[property][entity],
 		}
 	}
+	if pi != nil {
+		nextToken, err := util.EncodeProto(pi)
+		if err != nil {
+			return nil, err
+		}
+		res.NextToken = nextToken
+	}
+	return res, nil
+}
+
+// BulkTriples implements mixer.BulkTriples handler.
+func BulkTriples(
+	ctx context.Context,
+	in *pb.BulkTriplesRequest,
+	store *store.Store,
+) (*pb.BulkTriplesResponse, error) {
+	entities := in.GetEntities()
+	direction := in.GetDirection()
+	token := in.GetNextToken()
+	if direction != util.DirectionOut && direction != util.DirectionIn {
+		return nil, status.Errorf(
+			codes.InvalidArgument, "uri should be /v1/triples/out/ or /v1/triples/in/")
+	}
+	if !util.CheckValidDCIDs(entities) {
+		return nil, status.Errorf(
+			codes.InvalidArgument, "invalid entities %s", entities)
+	}
+	bulkPropsResp, err := properties.BulkProperties(
+		ctx, &pb.BulkPropertiesRequest{
+			Entities:  entities,
+			Direction: direction,
+		},
+		store,
+	)
+	if err != nil {
+		return nil, err
+	}
+	bulkProps := bulkPropsResp.GetData()
+	entityProps := map[string]map[string]struct{}{}
+	for _, e := range entities {
+		entityProps[e] = map[string]struct{}{}
+	}
+	properties := []string{}
+	for _, resp := range bulkProps {
+		for _, p := range resp.GetProperties() {
+			entityProps[resp.GetEntity()][p] = struct{}{}
+		}
+		properties = util.MergeDedupe(properties, resp.GetProperties())
+	}
+	data, pi, err := propertyvalues.Fetch(
+		ctx,
+		store,
+		properties,
+		entities,
+		0,
+		token,
+		direction,
+	)
+	if err != nil {
+		return nil, err
+	}
+	res := &pb.BulkTriplesResponse{
+		Data: []*pb.BulkTriplesResponse_EntityTriples{},
+	}
+	triplesByEntity := map[string]map[string][]*pb.EntityInfo{}
+	for _, e := range entities {
+		triplesByEntity[e] = map[string][]*pb.EntityInfo{}
+	}
+	for p := range data {
+		for e := range data[p] {
+			if _, ok := entityProps[e][p]; ok {
+				triplesByEntity[e][p] = data[p][e]
+			}
+		}
+	}
+	for e := range triplesByEntity {
+		entityTriples := &pb.BulkTriplesResponse_EntityTriples{
+			Entity:  e,
+			Triples: map[string]*pb.EntityInfoCollection{},
+		}
+		for p := range triplesByEntity[e] {
+			entityTriples.Triples[p] = &pb.EntityInfoCollection{
+				Entities: triplesByEntity[e][p],
+			}
+		}
+		res.Data = append(res.Data, entityTriples)
+	}
+
 	if pi != nil {
 		nextToken, err := util.EncodeProto(pi)
 		if err != nil {
