df072adbd3f3f7be8c2080316c837a0773efc60a
spaceenter <swyddz1989@gmail.com>
2022-09-11 22:39:18 -0700

    More comments

--- internal/server/v1/observations/derived_series.go
+++ internal/server/v1/observations/derived_series.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// API Implementation for /v1/derived/observations/series/...
+// Package observations implements for /v1/derived/observations/series/...
 package observations
 
 import (
