c77306d7ac4a2bbecf6ccf3532ce70d0ab127892
Bo Xu <boxu@google.com>
2022-09-21 21:09:30 -0700

    lint

--- internal/server/node/obs_triple.go
+++ internal/server/node/obs_triple.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package for node related operations
+// Package node holds helper functions for node related operations.
 package node
 
 import (
