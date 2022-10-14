df7ca2d09725ecd7256adefea2e04142c542e3a5
Bo Xu <shifucun@users.noreply.github.com>
2022-10-14 12:20:16 -0700

    resolve comments

--- internal/server/v1/observationdates/observation_dates_linked.go
+++ internal/server/v1/observationdates/observation_dates_linked.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// observationdates holds API implementation for observation dates.
+// Package observationdates holds API implementation for observation dates.
 
 package observationdates
 
