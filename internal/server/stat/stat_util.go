c19daaf65fb8d3cd65eaa6c8c2f8b31355c0d681
Bo Xu <shifucun@users.noreply.github.com>
2022-10-24 13:42:12 -0700

    prefer latest date

--- internal/server/stat/stat_util.go
+++ internal/server/stat/stat_util.go
@@ -247,18 +247,24 @@ func getSourceSeriesHash(series *pb.SourceSeries) string {
 }
 
 // CollectDistinctSourceSeries merges lists of SourceSeries.
-// For same source series, the one with more data points is used. In most cases,
-// this is the series with the latest data as well.
+// For same source series, use the seriers with most recent latest date. If the
+// latest dates are the same, choose based on the import order.
 func CollectDistinctSourceSeries(seriesList ...[]*pb.SourceSeries) []*pb.SourceSeries {
 	result := []*pb.SourceSeries{}
 	resultMap := map[string]*pb.SourceSeries{}
+	metahashToDate := map[string]string{}
 	for _, series := range seriesList {
 		for _, s := range series {
 			metahash := getSourceSeriesHash(s)
-			// The input series list is sorted, so the for loop is from the msot
-			// preferred sereis to the less preferred series
-			if _, ok := resultMap[metahash]; ok && len(s.Val) <= len(resultMap[metahash].Val) {
-				continue
+			if _, ok := resultMap[metahash]; ok {
+				if _, ok := metahashToDate[metahash]; !ok {
+					metahashToDate[metahash] = getLatestDate(resultMap[metahash].Val)
+				}
+				latestDate := getLatestDate(s.Val)
+				if latestDate <= metahashToDate[metahash] {
+					continue
+				}
+				metahashToDate[metahash] = latestDate
 			}
 			resultMap[metahash] = s
 		}
@@ -268,4 +274,14 @@ func CollectDistinctSourceSeries(seriesList ...[]*pb.SourceSeries) []*pb.SourceS
 	}
 	sort.Sort(ranking.SeriesByRank(result))
 	return result
+}
+
+func getLatestDate(val map[string]float64) string {
+	latestDate := ""
+	for date := range val {
+		if date > latestDate {
+			latestDate = date
+		}
+	}
+	return latestDate
 }
