804631d47acc8e9d3a97a4fcae2bfa493fb645eb
Bo Xu <shifucun@users.noreply.github.com>
2023-03-03 13:27:27 -0800

    lint

--- internal/server/v2/propertyvalues/api.go
+++ internal/server/v2/propertyvalues/api.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package propertyvalues is for V2 property values API
+// Package propertyvalues is for V2 property values API
 package propertyvalues
 
 import (
