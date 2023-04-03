cbd02bb83dff5cb7b4f454cb0460012f3c879498
William Apollo <4895865+Spaceenter@users.noreply.github.com>
2023-04-03 13:45:22 -0700

    implement property labels for V2 API. (#1078)

---
+++ internal/server/v2/properties/golden/properties_test.go
@@ -0,0 +1,85 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golden
+
+import (
+	"context"
+	"path"
+	"runtime"
+	"testing"
+
+	pbs "github.com/datacommonsorg/mixer/internal/proto/service"
+	pbv2 "github.com/datacommonsorg/mixer/internal/proto/v2"
+	"github.com/datacommonsorg/mixer/test"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestPropertiesV2(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	_, filename, _, _ := runtime.Caller(0)
+	goldenPath := path.Join(path.Dir(filename), "properties")
+
+	testSuite := func(mixer pbs.MixerClient, latencyTest bool) {
+		for _, c := range []struct {
+			goldenFile string
+			nodes      []string
+			property   string
+		}{
+			{
+				"out.json",
+				[]string{"geoId/06", "bio/hs"},
+				"->",
+			},
+			{
+				"in.json",
+				[]string{"geoId/06085"},
+				"<-",
+			},
+		} {
+			req := &pbv2.NodeRequest{
+				Nodes:    c.nodes,
+				Property: c.property,
+			}
+			resp, err := mixer.V2Node(ctx, req)
+			if err != nil {
+				t.Errorf("could not run mixer.QueryV2: %s", err)
+				continue
+			}
+			if latencyTest {
+				continue
+			}
+			if test.GenerateGolden {
+				test.UpdateProtoGolden(resp, goldenPath, c.goldenFile)
+				continue
+			}
+			var expected pbv2.NodeResponse
+			if err := test.ReadJSON(goldenPath, c.goldenFile, &expected); err != nil {
+				t.Errorf("Can not Unmarshal golden file %s: %v", c.goldenFile, err)
+				continue
+			}
+			if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
+				t.Errorf("Golden got diff: %v", diff)
+				continue
+			}
+		}
+	}
+	if err := test.TestDriver(
+		"PropertiesV2", &test.TestOption{}, testSuite); err != nil {
+		t.Errorf("TestDriver() = %s", err)
+	}
+}
