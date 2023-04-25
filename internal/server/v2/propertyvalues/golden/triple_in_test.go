7ae5af20279ce1fba0dcf6a3ad96e86549ffc7b1
William Apollo <4895865+Spaceenter@users.noreply.github.com>
2023-04-24 21:34:55 -0700

    Add triples to V2 API. (#1125)

---
+++ internal/server/v2/propertyvalues/golden/triple_in_test.go
@@ -0,0 +1,89 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golden
+
+import (
+	"context"
+	"path"
+	"runtime"
+	"testing"
+
+	pbs "github.com/datacommonsorg/mixer/internal/proto/service"
+	pbv2 "github.com/datacommonsorg/mixer/internal/proto/v2"
+	"github.com/datacommonsorg/mixer/test"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestTripleIn(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	_, filename, _, _ := runtime.Caller(0)
+	goldenPath := path.Join(path.Dir(filename), "triple_in")
+
+	testSuite := func(mixer pbs.MixerClient, latencyTest bool) {
+		for _, c := range []struct {
+			goldenFile string
+			nodes      []string
+			property   string
+			token      string
+		}{
+			{
+				"result1.json",
+				[]string{"Country", "BiologicalSpecimen", "Count_Person", "dummy"},
+				"<-*",
+				"",
+			},
+			{
+				"result2.json",
+				[]string{"Country", "BiologicalSpecimen", "Count_Person", "dummy"},
+				"<-*",
+				"H4sIAAAAAAAA/2zNMcvCQAzG8bfX60sIDiWCH0gd3BQKLg5yliiBXlKu6eC3lxbcXJ7lgd8fT0h7scFe0qehG7mXzIr//h75/Pz10R8FqChCaCsKUFOASAEavOHmYLP6/cJlMsVd5jTNhTOrH1ktiya3gtvOk8vkC3pNRdJjYFqwCFX7hSPU68Y10nwAAAD//wEAAP//qyVVPKcAAAA=",
+			},
+		} {
+			req := &pbv2.NodeRequest{
+				Nodes:     c.nodes,
+				Property:  c.property,
+				NextToken: c.token,
+			}
+			resp, err := mixer.V2Node(ctx, req)
+			if err != nil {
+				t.Errorf("could not run V2Node: %s", err)
+				continue
+			}
+			if latencyTest {
+				continue
+			}
+			if test.GenerateGolden {
+				test.UpdateProtoGolden(resp, goldenPath, c.goldenFile)
+				continue
+			}
+			var expected pbv2.NodeResponse
+			if err := test.ReadJSON(goldenPath, c.goldenFile, &expected); err != nil {
+				t.Errorf("Can not Unmarshal golden file %s: %v", c.goldenFile, err)
+				continue
+			}
+			if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
+				t.Errorf("Golden got diff: %v", diff)
+				continue
+			}
+		}
+	}
+	if err := test.TestDriver(
+		"TestTripleIn", &test.TestOption{}, testSuite); err != nil {
+		t.Errorf("TestDriver() = %s", err)
+	}
+}
---
+++ internal/server/v2/propertyvalues/golden/triple_out_test.go
@@ -0,0 +1,80 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golden
+
+import (
+	"context"
+	"path"
+	"runtime"
+	"testing"
+
+	pbs "github.com/datacommonsorg/mixer/internal/proto/service"
+	pbv2 "github.com/datacommonsorg/mixer/internal/proto/v2"
+	"github.com/datacommonsorg/mixer/test"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestTripleOut(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	_, filename, _, _ := runtime.Caller(0)
+	goldenPath := path.Join(path.Dir(filename), "triple_out")
+
+	testSuite := func(mixer pbs.MixerClient, latencyTest bool) {
+		for _, c := range []struct {
+			goldenFile string
+			nodes      []string
+			property   string
+		}{
+			{
+				"result.json",
+				[]string{"Country", "BiologicalSpecimen", "Count_Person", "dc/o/vs51dzghn79eg"},
+				"->*",
+			},
+		} {
+			req := &pbv2.NodeRequest{
+				Nodes:    c.nodes,
+				Property: c.property,
+			}
+			resp, err := mixer.V2Node(ctx, req)
+			if err != nil {
+				t.Errorf("could not run V2Node: %s", err)
+				continue
+			}
+			if latencyTest {
+				continue
+			}
+			if test.GenerateGolden {
+				test.UpdateProtoGolden(resp, goldenPath, c.goldenFile)
+				continue
+			}
+			var expected pbv2.NodeResponse
+			if err := test.ReadJSON(goldenPath, c.goldenFile, &expected); err != nil {
+				t.Errorf("Can not Unmarshal golden file %s: %v", c.goldenFile, err)
+				continue
+			}
+			if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
+				t.Errorf("Golden got diff: %v", diff)
+				continue
+			}
+		}
+	}
+	if err := test.TestDriver(
+		"TestTripleOut", &test.TestOption{}, testSuite); err != nil {
+		t.Errorf("TestDriver() = %s", err)
+	}
+}
