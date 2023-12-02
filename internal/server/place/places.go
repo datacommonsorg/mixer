da08d0a025ba2b6aa17f7f7b1d0b5100a93d8893
Bo Xu <shifucun@users.noreply.github.com>
2023-12-02 01:20:36 +0000

    Fix redundant place info data processing (#1305)
    There is no need to traverse the parent tree when merging import groups.
    The original cache has obtained ALL the parent places so just deduping
    among import group is sufficient.

--- internal/server/place/places.go
+++ internal/server/place/places.go
@@ -16,6 +16,7 @@ package place
 
 import (
 	"context"
+	"time"
 
 	"github.com/datacommonsorg/mixer/internal/store/bigtable"
 	"google.golang.org/protobuf/proto"
@@ -189,6 +190,7 @@ func GetPlaceMetadataHelper(
 	entities []string,
 	store *store.Store,
 ) (map[string]*pb.PlaceMetadata, error) {
+	defer util.TimeTrack(time.Now(), "GetPlaceMetadataHelper")
 	// Place metadata are from base geo imports. Only trust the base cache.
 	btDataList, err := bigtable.Read(
 		ctx,
@@ -207,66 +209,59 @@ func GetPlaceMetadataHelper(
 		return nil, err
 	}
 	result := map[string]*pb.PlaceMetadata{}
-	metaMap := map[string]*pb.PlaceMetadataCache_PlaceInfo{}
+	infoMap := map[string]*pb.PlaceMetadata_PlaceInfo{}
+	parentMap := map[string]map[string]struct{}{}
+	directParents := map[string][]string{}
 	for _, btData := range btDataList {
 		for _, row := range btData {
 			raw, ok := row.Data.(*pb.PlaceMetadataCache)
 			if !ok {
 				continue
 			}
-			for _, info := range raw.Places {
-				if currInfo, ok := metaMap[info.Dcid]; !ok {
-					metaMap[info.Dcid] = info
+			currPlace := row.Parts[0]
+			if _, ok := result[currPlace]; !ok {
+				result[currPlace] = &pb.PlaceMetadata{}
+			}
+			if _, ok := parentMap[currPlace]; !ok {
+				parentMap[currPlace] = map[string]struct{}{}
+			}
+			for _, place := range raw.Places {
+				if _, ok := infoMap[place.Dcid]; !ok {
+					infoMap[place.Dcid] = &pb.PlaceMetadata_PlaceInfo{
+						Dcid: place.Dcid,
+						Name: place.Name,
+						Type: place.Type,
+					}
+				}
+				directParents[place.Dcid] = place.Parents
+				if place.Dcid == currPlace {
+					result[currPlace].Self = infoMap[currPlace]
 				} else {
-					// Merge place info from new import group.
-					if currInfo.Name == "" {
-						currInfo.Name = info.Name
-					}
-					if currInfo.Type == "" {
-						currInfo.Type = info.Type
-					}
-					currInfo.Parents = append(currInfo.Parents, info.Parents...)
+					parentMap[currPlace][place.Dcid] = struct{}{}
 				}
 			}
 		}
 	}
-	for _, entity := range entities {
-		entInfo, ok := metaMap[entity]
-		if !ok {
-			continue
-		}
-		processed := pb.PlaceMetadata{}
-		processed.Self = &pb.PlaceMetadata_PlaceInfo{
-			Dcid: entInfo.Dcid,
-			Name: entInfo.Name,
-			Type: entInfo.Type,
-		}
-		visited := map[string]struct{}{}
-		parents := entInfo.Parents
-		for {
-			if len(parents) == 0 {
-				break
+	for dcid, allParents := range parentMap {
+		// Order direct parents at the beginning of the list.
+		childPlaces := []string{dcid}
+		for len(childPlaces) > 0 {
+			newChildPlaces := []string{}
+			for _, child := range childPlaces {
+				for _, parent := range directParents[child] {
+					if _, ok := allParents[parent]; !ok {
+						continue
+					}
+					result[dcid].Parents = append(result[dcid].Parents, infoMap[parent])
+					newChildPlaces = append(newChildPlaces, parent)
+					delete(allParents, parent)
+				}
 			}
-			curr := parents[0]
-			parents = parents[1:]
-			if _, ok := visited[curr]; ok {
-				continue
-			}
-			// To handle potential data issue in the cache, where parent node
-			// is not in the PlaceInfo.Places field.
-			info, ok := metaMap[curr]
-			if !ok {
-				continue
-			}
-			processed.Parents = append(processed.Parents, &pb.PlaceMetadata_PlaceInfo{
-				Dcid: curr,
-				Name: info.Name,
-				Type: info.Type,
-			})
-			visited[curr] = struct{}{}
-			parents = append(parents, info.Parents...)
+			childPlaces = newChildPlaces
 		}
-		result[entity] = &processed
+		for parent := range allParents {
+			result[dcid].Parents = append(result[dcid].Parents, infoMap[parent])
+		}
 	}
 	return result, nil
 }
