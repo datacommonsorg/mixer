85af98c606f19a18315fbd47b6410f5c77e2bace
natalie <77713883+n-h-diaz@users.noreply.github.com>
2024-05-31 07:20:50 -0700

    Add basic support for calculations (#1364)
    This also splits up the fetching and merging for the formulas (follow up
    to https://github.com/datacommonsorg/mixer/pull/1362) and adds basic
    tests.
    
    This currently does not support matching facets, which will be a follow
    up.
    
    Currently, this independently calculates each missing variable/entity
    pair, rather than combining the entities into one DerivedSeries request,
    and then updates the merging to accept an array of responses. This was
    to avoid computing the calculation if some of the entities already have
    data, however if it's preferred to only do one DerivedSeries per formula
    I can update this.

---
+++ internal/server/v2/observation/golden/calculation_test.go
@@ -0,0 +1,92 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golden
+
+import (
+	"context"
+	"path"
+	"runtime"
+	"testing"
+
+	pbs "github.com/datacommonsorg/mixer/internal/proto/service"
+	pbv2 "github.com/datacommonsorg/mixer/internal/proto/v2"
+	"github.com/datacommonsorg/mixer/test"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestCalculation(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	_, filename, _, _ := runtime.Caller(0)
+	goldenPath := path.Join(path.Dir(filename), "calculation")
+
+	testSuite := func(mixer pbs.MixerClient, latencyTest bool) {
+		for _, c := range []struct {
+			variables  []string
+			entities   []string
+			date       string
+			filter     *pbv2.FacetFilter
+			goldenFile string
+		}{
+			{
+				[]string{
+					"Count_Person_Female", "Count_Person_Male", "Count_WetBulbTemperatureEvent",
+				},
+				[]string{"wikidataId/Q613"},
+				"",
+				nil,
+				"statistical_calculation.json",
+			},
+		} {
+			goldenFile := c.goldenFile
+			resp, err := mixer.V2Observation(ctx, &pbv2.ObservationRequest{
+				Select:   []string{"variable", "entity", "date", "value"},
+				Variable: &pbv2.DcidOrExpression{Dcids: c.variables},
+				Entity:   &pbv2.DcidOrExpression{Dcids: c.entities},
+				Date:     c.date,
+				Filter:   c.filter,
+			})
+			if err != nil {
+				t.Errorf("could not run V2Observation (direct): %s", err)
+				continue
+			}
+			if latencyTest {
+				continue
+			}
+			if test.GenerateGolden {
+				test.UpdateGolden(resp, goldenPath, goldenFile)
+				continue
+			}
+			var expected pbv2.ObservationResponse
+			if err = test.ReadJSON(goldenPath, goldenFile, &expected); err != nil {
+				t.Errorf("Can not Unmarshal golden file: %s", err)
+				continue
+			}
+			if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
+				t.Errorf("payload got diff: %v", diff)
+				continue
+			}
+		}
+	}
+	if err := test.TestDriver(
+		"TestCalculation",
+		&test.TestOption{UseSQLite: true, CacheSVFormula: true},
+		testSuite,
+	); err != nil {
+		t.Errorf("TestDriver() = %s", err)
+	}
+}
