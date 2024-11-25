b5d6d7ced9b08161b4f9a191889118c2e4ecd285
Gabriel Mechali <gabriel.mechali@gmail.com>
2024-11-25 16:51:16 -0500

    Fix the posisble index out of bounds error on stripping quotes. (#1471)
    Adds a length check to not try to strip quotes when the string is just a
    "

--- internal/translator/translate.go
+++ internal/translator/translate.go
@@ -623,7 +623,7 @@ func removeConstraints(
 }
 
 func StripQuotes(str string) string {
-	if (strings.HasPrefix(str, "\"")) && (strings.HasSuffix(str, "\"")) {
+	if (len(str) > 1) && (strings.HasPrefix(str, "\"")) && (strings.HasSuffix(str, "\"")) {
 		str = str[1 : len(str)-1]
 	}
 	return str
