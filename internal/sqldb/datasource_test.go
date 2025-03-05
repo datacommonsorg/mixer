9d508d3a39692c2ba1d196225c8d653eec121f58
Keyur Shah <keyurs@google.com>
2025-03-05 15:48:47 -0800

    Update test to test datasource id in addition to client id.

--- internal/sqldb/datasource_test.go
+++ internal/sqldb/datasource_test.go
@@ -27,4 +27,7 @@ func TestId(t *testing.T) {
 	}
 
 	assert.Equal(t, "sqlite-../../test/sqlquery/key_value/datacommons.db", sqlClient.id)
+
+	SQLDataSource := NewSQLDataSource(sqlClient)
+	assert.Equal(t, "sql-sqlite-../../test/sqlquery/key_value/datacommons.db", SQLDataSource.Id())
 }
