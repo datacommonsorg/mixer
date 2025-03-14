d452079993a6d0b6b0ebe57cf4396e3f381922b4
Natalie Diaz <nhdiaz@google.com>
2025-03-14 10:01:38 +0530

    V3 node chain fix

--- internal/server/spanner/statements.go
+++ internal/server/spanner/statements.go
@@ -53,261 +53,254 @@ var statements = struct {
 	filterTypes string
 }{
 	getPropsBySubjectID: `
-		GRAPH DCGraph MATCH -[e:Edge
-		WHERE 
-			e.subject_id IN UNNEST(@ids)]->
-		RETURN DISTINCT
-			e.subject_id,
-			e.predicate
-		ORDER BY
-			e.subject_id,
-			e.predicate
-	`,
+    GRAPH DCGraph MATCH -[e:Edge
+    WHERE 
+      e.subject_id IN UNNEST(@ids)]->
+    RETURN DISTINCT
+      e.subject_id,
+      e.predicate
+    ORDER BY
+      e.subject_id,
+      e.predicate
+  `,
 	getPropsByObjectID: `
-		GRAPH DCGraph MATCH -[e:Edge
-		WHERE
-			e.object_id IN UNNEST(@ids)
-			AND e.object_value IS NULL
-		]->
-		RETURN DISTINCT
-			e.object_id AS subject_id,
-			e.predicate
-		ORDER BY
-			subject_id,
-			predicate
-	`,
+    GRAPH DCGraph MATCH -[e:Edge
+    WHERE
+      e.object_id IN UNNEST(@ids)
+      AND e.object_value IS NULL
+    ]->
+    RETURN DISTINCT
+      e.object_id AS subject_id,
+      e.predicate
+    ORDER BY
+      subject_id,
+      predicate
+  `,
 	getEdgesBySubjectID: `
-		GRAPH DCGraph MATCH -[e:Edge
-		WHERE
-			e.subject_id IN UNNEST(@ids)
-			AND e.object_value IS NULL%[1]s]->(n:Node)
-		RETURN 
-			e.subject_id,
-			e.predicate,
-			e.object_id,
-			'' as object_value,
-			COALESCE(e.provenance, '') AS provenance,
-			COALESCE(n.name, '') AS name,
-			COALESCE(n.types, []) AS types
-		UNION ALL
-		MATCH -[e:Edge
-		WHERE
-			e.subject_id IN UNNEST(@ids)
-			AND e.object_value IS NOT NULL%[1]s]-> 
-		RETURN 
-			e.subject_id,
-			e.predicate,
-			'' as object_id,
-			e.object_value,
-			e.provenance,
-			'' AS name,
-			ARRAY<STRING>[] AS types
-		NEXT
-		RETURN
-			subject_id,
-			predicate,
-			object_id,
-			object_value,
-			provenance,
-			name,
-			types
-		ORDER BY
-			subject_id,
-			predicate,
-			object_id,
-			object_value,
-			provenance
-	`,
+    GRAPH DCGraph MATCH -[e:Edge
+    WHERE
+      e.subject_id IN UNNEST(@ids)
+      AND e.object_value IS NULL%[1]s]->(n:Node)
+    RETURN 
+      e.subject_id,
+      e.predicate,
+      e.object_id,
+      '' as object_value,
+      COALESCE(e.provenance, '') AS provenance,
+      COALESCE(n.name, '') AS name,
+      COALESCE(n.types, []) AS types
+    UNION ALL
+    MATCH -[e:Edge
+    WHERE
+      e.subject_id IN UNNEST(@ids)
+      AND e.object_value IS NOT NULL%[1]s]-> 
+    RETURN 
+      e.subject_id,
+      e.predicate,
+      '' as object_id,
+      e.object_value,
+      e.provenance,
+      '' AS name,
+      ARRAY<STRING>[] AS types
+    NEXT
+    RETURN
+      subject_id,
+      predicate,
+      object_id,
+      object_value,
+      provenance,
+      name,
+      types
+    ORDER BY
+      subject_id,
+      predicate,
+      object_id,
+      object_value,
+      provenance
+  `,
 	getChainedEdgesBySubjectID: fmt.Sprintf(`
-		GRAPH DCGraph MATCH ANY (m:Node
-		WHERE
-			m.subject_id IN UNNEST(@ids))-[e:Edge
-		WHERE
-			e.predicate = @predicate]->{1,%d}(n:Node)
-		WHERE 
-			m != n
-		RETURN 
-			m.subject_id,
-			n.subject_id as object_id,
-			'' as object_value,
-			COALESCE(n.name, '') AS name,
-			COALESCE(n.types, []) AS types
-		UNION ALL
-		MATCH -[e:Edge
-		WHERE
-			e.subject_id IN UNNEST(@ids)
-			AND e.object_value IS NOT NULL
-			AND e.predicate = @predicate]-> 
-		RETURN 
-			e.subject_id,
-			'' AS object_id,
-			e.object_value,
-			'' AS name,
-			ARRAY<STRING>[] AS types
-		NEXT
-		RETURN
-			subject_id,
-			@result_predicate AS predicate,
-			object_id,
-			object_value,
-			'' AS provenance,
-			name, 
-			types
-		ORDER BY
-			subject_id,
-			predicate,
-			object_id,
-			object_value
-	`, MAX_HOPS),
+    GRAPH DCGraph MATCH ANY (m:Node
+    WHERE
+      m.subject_id IN UNNEST(@ids))-[e:Edge
+    WHERE
+      e.predicate = @predicate]->{1,%d}(n:Node)
+    WHERE 
+      m != n
+    RETURN 
+      m.subject_id,
+      n.subject_id as object_id,
+      '' as object_value,
+      COALESCE(n.name, '') AS name,
+      COALESCE(n.types, []) AS types
+    UNION ALL
+    MATCH -[e:Edge
+    WHERE
+      e.subject_id IN UNNEST(@ids)
+      AND e.object_value IS NOT NULL
+      AND e.predicate = @predicate]-> 
+    RETURN 
+      e.subject_id,
+      '' AS object_id,
+      e.object_value,
+      '' AS name,
+      ARRAY<STRING>[] AS types
+    NEXT
+    RETURN
+      subject_id,
+      @result_predicate AS predicate,
+      object_id,
+      object_value,
+      '' AS provenance,
+      name, 
+      types
+    ORDER BY
+      subject_id,
+      predicate,
+      object_id,
+      object_value
+  `, MAX_HOPS),
 	getEdgesByObjectID: `
-		GRAPH DCGraph MATCH <-[e:Edge
-		WHERE
-			e.object_id IN UNNEST(@ids)
-			AND e.subject_id != e.object_id%s]-(n:Node) 
-		RETURN 
-			e.object_id AS subject_id,
-			e.predicate,
-			e.subject_id AS object_id,
-			'' AS object_value,
-			COALESCE(e.provenance, '') AS provenance,
-			COALESCE(n.name, '') AS name,
-			COALESCE(n.types, []) AS types
-		ORDER BY
-			subject_id,
-			predicate,
-			object_id
-	`,
+    GRAPH DCGraph MATCH <-[e:Edge
+    WHERE
+      e.object_id IN UNNEST(@ids)
+      AND e.subject_id != e.object_id%s]-(n:Node) 
+    RETURN 
+      e.object_id AS subject_id,
+      e.predicate,
+      e.subject_id AS object_id,
+      '' AS object_value,
+      COALESCE(e.provenance, '') AS provenance,
+      COALESCE(n.name, '') AS name,
+      COALESCE(n.types, []) AS types
+    ORDER BY
+      subject_id,
+      predicate,
+      object_id
+  `,
 	getChainedEdgesByObjectID: fmt.Sprintf(`
-		GRAPH DCGraph MATCH ANY (m:Node
-		WHERE 
-			m.subject_id IN UNNEST(@ids))<-[e:Edge
-		WHERE
-			e.predicate = @predicate]-{1,%d}(n:Node) 
-		WHERE
-			m!= n	
-		RETURN 
-			m.subject_id,
-			n.subject_id AS object_id,
-			COALESCE(n.name, '') AS name,
-			COALESCE(n.types, []) AS types
-		NEXT
-		RETURN
-			subject_id, 
-			@result_predicate AS predicate,
-			object_id,
-			'' AS object_value,
-			'' AS provenance, 
-			name, 
-			types
-		ORDER BY
-			subject_id,
-			predicate,
-			object_id
-		`, MAX_HOPS),
+    GRAPH DCGraph MATCH ANY (m:Node
+    WHERE 
+      m.subject_id IN UNNEST(@ids))<-[e:Edge
+    WHERE
+      e.predicate = @predicate]-{1,%d}(n:Node) 
+    WHERE
+      m != n  
+    RETURN 
+      m.subject_id,
+      n.subject_id AS object_id,
+      COALESCE(n.name, '') AS name,
+      COALESCE(n.types, []) AS types
+    NEXT
+    RETURN
+      subject_id, 
+      @result_predicate AS predicate,
+      object_id,
+      '' AS object_value,
+      '' AS provenance, 
+      name, 
+      types
+    ORDER BY
+      subject_id,
+      predicate,
+      object_id
+    `, MAX_HOPS),
 	filterProps: `
-		AND e.predicate IN UNNEST(@props)
-	`,
+    AND e.predicate IN UNNEST(@props)
+  `,
 	filterObjects: `
-		NEXT 
-		MATCH -[e:Edge 
-		WHERE
-			e.predicate = @prop%[1]d
-			AND (
-				e.object_id IN UNNEST(@val%[1]d)
-				OR e.object_value IN UNNEST(@val%[1]d)
-			)]-> 
-		WHERE
-			e.subject_id = object_id
-		RETURN
-			subject_id,
-			predicate,
-			object_id,
-			object_value,
-			provenance,
-			name,
-			types			
-	`,
+    NEXT 
+    MATCH -[filter:Edge 
+    WHERE
+      filter.predicate = @prop%[1]d
+      AND (
+        filter.object_id IN UNNEST(@val%[1]d)
+        OR filter.object_value IN UNNEST(@val%[1]d)
+      )]-> 
+    WHERE
+      filter.subject_id = object_id
+    RETURN
+      subject_id,
+      predicate,
+      object_id,
+      object_value,
+      provenance,
+      name,
+      types     
+  `,
 	getObsByVariableAndEntity: `
-		SELECT
-			variable_measured,
-			observation_about,
-			observations,
-			provenance,
-			COALESCE(observation_period, '') AS observation_period,
-			COALESCE(measurement_method, '') AS measurement_method,
-			COALESCE(unit, '') AS unit,
-			COALESCE(scaling_factor, '') AS scaling_factor,
-			import_name,
-			provenance_url
-		FROM Observation
-		WHERE
-			variable_measured IN UNNEST(@variables) AND
-			observation_about IN UNNEST(@entities)
-	`,
+    SELECT
+      variable_measured,
+      observation_about,
+      observations,
+      provenance,
+      COALESCE(observation_period, '') AS observation_period,
+      COALESCE(measurement_method, '') AS measurement_method,
+      COALESCE(unit, '') AS unit,
+      COALESCE(scaling_factor, '') AS scaling_factor,
+      import_name,
+      provenance_url
+    FROM Observation
+    WHERE
+      variable_measured IN UNNEST(@variables) AND
+      observation_about IN UNNEST(@entities)
+  `,
 	getObsByVariableAndContainedInPlace: `
-		SELECT
-			obs.variable_measured,
-			obs.observation_about,
-			obs.observations,
-			obs.provenance,
-			COALESCE(obs.observation_period, '') AS observation_period,
-			COALESCE(obs.measurement_method, '') AS measurement_method,
-			COALESCE(obs.unit, '') AS unit,
-			COALESCE(obs.scaling_factor, '') AS scaling_factor,
-			obs.import_name, 
-			obs.provenance_url
-		FROM GRAPH_TABLE (
-				DCGraph MATCH <-[e:Edge
-				WHERE
-					e.object_id = @ancestor
-					AND e.subject_id != e.object_id
-					AND e.predicate = 'linkedContainedInPlace']-
-				RETURN 
-				e.subject_id as object_id
-				NEXT
-				MATCH -[e:Edge 
-				WHERE
-					e.predicate = 'typeOf'
-					AND e.object_id = @childPlaceType]-> 
-				WHERE e.subject_id = object_id
-				RETURN object_id
-			)result
-		INNER JOIN (
-		SELECT
-			*
-		FROM Observation
-		WHERE
-			variable_measured IN UNNEST(@variables))obs
-		ON 
-			result.object_id = obs.observation_about
-	`,
+    SELECT
+      obs.variable_measured,
+      obs.observation_about,
+      obs.observations,
+      obs.provenance,
+      COALESCE(obs.observation_period, '') AS observation_period,
+      COALESCE(obs.measurement_method, '') AS measurement_method,
+      COALESCE(obs.unit, '') AS unit,
+      COALESCE(obs.scaling_factor, '') AS scaling_factor,
+      obs.import_name, 
+      obs.provenance_url
+    FROM GRAPH_TABLE (
+        DCGraph MATCH <-[e:Edge
+        WHERE
+          e.object_id = @ancestor
+          AND e.subject_id != e.object_id
+          AND e.predicate = 'linkedContainedInPlace']-()-[{predicate: 'typeOf', object_id: @childPlaceType}]->
+        RETURN 
+        e.subject_id as object_id
+      )result
+    INNER JOIN (
+    SELECT
+      *
+    FROM Observation
+    WHERE
+      variable_measured IN UNNEST(@variables))obs
+    ON 
+      result.object_id = obs.observation_about
+  `,
 	searchNodesByQuery: fmt.Sprintf(`
-		GRAPH DCGraph
-		MATCH (n:Node)
-		WHERE 
-			SEARCH(n.name_tokenlist, @query)
-		RETURN n.subject_id, n.name, n.types, SCORE(n.name_tokenlist, @query, enhance_query => TRUE) AS score 
-		ORDER BY score + IF(n.name = @query, 1, 0) DESC, n.name ASC
-		LIMIT %d
-	`, merger.MAX_SEARCH_RESULTS),
+    GRAPH DCGraph
+    MATCH (n:Node)
+    WHERE 
+      SEARCH(n.name_tokenlist, @query)
+    RETURN n.subject_id, n.name, n.types, SCORE(n.name_tokenlist, @query, enhance_query => TRUE) AS score 
+    ORDER BY score + IF(n.name = @query, 1, 0) DESC, n.name ASC
+    LIMIT %d
+  `, merger.MAX_SEARCH_RESULTS),
 	searchNodesByQueryAndTypes: fmt.Sprintf(`
-		GRAPH DCGraph
-		MATCH (n:Node)
-		WHERE 
-			SEARCH(n.name_tokenlist, @query)
-			AND ARRAY_INCLUDES_ANY(n.types, @types)
-		RETURN n.subject_id, n.name, n.types, SCORE(n.name_tokenlist, @query, enhance_query => TRUE) AS score
-		ORDER BY score + IF(n.name = @query, 1, 0) DESC, n.name ASC
-		LIMIT %d
-	`, merger.MAX_SEARCH_RESULTS),
+    GRAPH DCGraph
+    MATCH (n:Node)
+    WHERE 
+      SEARCH(n.name_tokenlist, @query)
+      AND ARRAY_INCLUDES_ANY(n.types, @types)
+    RETURN n.subject_id, n.name, n.types, SCORE(n.name_tokenlist, @query, enhance_query => TRUE) AS score
+    ORDER BY score + IF(n.name = @query, 1, 0) DESC, n.name ASC
+    LIMIT %d
+  `, merger.MAX_SEARCH_RESULTS),
 	searchObjectValues: `
-		GRAPH DCGraph 
-		MATCH -[e:Edge 
-			WHERE e.predicate IN UNNEST(@predicates) AND SEARCH(e.object_value_tokenlist, @query)
-		]->(n:Node %s)
-		RETURN n.subject_id, n.name, n.types, e.predicate AS predicate, e.object_value AS object_value, SCORE(e.object_value_tokenlist, @query, enhance_query => TRUE) AS score
-		ORDER BY score + IF(e.object_value = @query, 1, 0) + IF(REGEXP_CONTAINS(n.subject_id, @query), 0.5, 0) desc, n.name ASC
-		LIMIT %d
-	`,
+    GRAPH DCGraph 
+    MATCH -[e:Edge 
+      WHERE e.predicate IN UNNEST(@predicates) AND SEARCH(e.object_value_tokenlist, @query)
+    ]->(n:Node %s)
+    RETURN n.subject_id, n.name, n.types, e.predicate AS predicate, e.object_value AS object_value, SCORE(e.object_value_tokenlist, @query, enhance_query => TRUE) AS score
+    ORDER BY score + IF(e.object_value = @query, 1, 0) + IF(REGEXP_CONTAINS(n.subject_id, @query), 0.5, 0) desc, n.name ASC
+    LIMIT %d
+  `,
 	filterTypes: `WHERE ARRAY_INCLUDES_ANY(n.types, @types)`,
 }
