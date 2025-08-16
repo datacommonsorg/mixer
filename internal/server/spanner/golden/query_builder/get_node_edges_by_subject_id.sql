430512d7241b301b9a9964ba13e554d8f29de9ed
natalie <77713883+n-h-diaz@users.noreply.github.com>
2025-08-16 10:43:16 -0700

    Add tests for query generation (#1595)
    Separates out spanner query generation so it can be tested independently
    from the query results (follow up to comments on
    https://github.com/datacommonsorg/mixer/pull/1591)
    
    Currently the tests use the same examples as query_test.go
    
    Also adds sorting to node filters for deterministic responses

---
+++ internal/server/spanner/golden/query_builder/get_node_edges_by_subject_id.sql
@@ -0,0 +1,17 @@
+		GRAPH DCGraph MATCH (m:Node
+		WHERE
+			m.subject_id IN ('Aadhaar','Monthly_Average_RetailPrice_Electricity_Residential','foo'))-[e:Edge]->(n:Node)
+		RETURN
+			m.subject_id,
+			e.predicate,
+			e.provenance,
+			n.value,
+			n.bytes,
+			n.name,
+			n.types
+		ORDER BY
+			subject_id,
+			predicate,
+			value,
+			provenance
+		LIMIT 5001
