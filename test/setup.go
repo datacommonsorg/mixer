7a65fdec0a203934a0e519243ecd8af44dad0d94
Natalie Diaz <nhdiaz@google.com>
2025-09-25 13:41:02 -0700

    fix

--- test/setup.go
+++ test/setup.go
@@ -72,7 +72,7 @@ var (
 	// Currently this is only enabled on workstations of developers working on the spanner graph POC.
 	// This ensures that the spanner tests don't impact existing tests while in the POC phase.
 	// TODO: Remove this variable after POC.
-	EnableSpannerGraph = true // os.Getenv("ENABLE_SPANNER_GRAPH") == "true"
+	EnableSpannerGraph = os.Getenv("ENABLE_SPANNER_GRAPH") == "true"
 )
 
 // This test runs agains staging staging bt and bq dataset.
@@ -210,7 +210,6 @@ func setupInternal(
 	}
 
 	dataSources := datasources.NewDataSources(sources)
-
 	// Processors
 	processors := []*dispatcher.Processor{}
 	if enableV3 {
