c9dd921de6530440bdb7f4c18661f94b11acf140
Keyur Shah <keyurs@google.com>
2026-03-13 11:34:12 -0700

    Regenerate query builder golden

--- internal/server/spanner/golden/query_builder/get_event_collection_date.sql
+++ internal/server/spanner/golden/query_builder/get_event_collection_date.sql
@@ -1,3 +1,4 @@
+		@{force_join_order=true}
 		GRAPH DCGraph MATCH (event:Node)-[:Edge {predicate: 'affectedPlace', object_id: 'country/LBR'}]->(), (event)-[:Edge {predicate: 'typeOf', object_id: 'FireEvent'}]->(), (event)-[:Edge {predicate: 'startDate'}]->(dateNode:Node)
 		RETURN DISTINCT 
 			SUBSTR(dateNode.value, 1, 7) AS month
