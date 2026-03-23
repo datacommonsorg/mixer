7b50ecfdb1a862757e078b957f2bad4f6e34e9de
Keyur Shah <keyurs@google.com>
2026-03-23 15:44:40 -0700

    Update Spanner queries to optionally fetch magnitude values

--- internal/server/spanner/golden/query_builder/get_event_collection_dcids_flood.sql
+++ internal/server/spanner/golden/query_builder/get_event_collection_dcids_flood.sql
@@ -1,6 +1,8 @@
 		@{force_join_order=true}
 		GRAPH DCGraph MATCH (event:Node)-[:Edge {predicate: 'affectedPlace', object_id: 'country/GBR'}]->(), (event)-[:Edge {predicate: 'typeOf', object_id: 'FloodEvent'}]->(), (event)-[:Edge {predicate: 'startDate'}]->(dateNode:Node)
+		OPTIONAL MATCH (event)-[:Edge {predicate: 'area'}]->(magNode:Node)
 		WHERE 
 			SUBSTR(dateNode.value, 1, 7) = '2025-01'
 		RETURN DISTINCT 
-			event.subject_id AS dcid
+			event.subject_id AS dcid,
+			magNode.value AS magnitude
