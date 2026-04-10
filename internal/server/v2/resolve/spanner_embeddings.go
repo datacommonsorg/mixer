c604c2d866c9c92042173ee891d3b049bcedca78
shixiao-coder <shixiao@google.com>
2026-04-10 10:44:26 -0400

    Add configuration to control spanner graph search

---
+++ internal/server/v2/resolve/spanner_embeddings.go
@@ -0,0 +1,43 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resolve
+
+type SearchAlgorithm string
+
+const (
+	VectorSearch   SearchAlgorithm = "vector_search"
+	FullTextSearch SearchAlgorithm = "full_text_search"
+	HybridSearch   SearchAlgorithm = "hybrid_search"
+)
+
+type GraphConfig string
+
+const (
+	GraphConfigDefault        GraphConfig = "default"
+	GraphConfigReranking      GraphConfig = "reranking"
+	GraphConfigGraphTraversal GraphConfig = "graph_traversal"
+)
+
+type EmbeddingConfig struct {
+	EmbeddingModel string `json:"embedding_model"`
+	EmbeddingType  string `json:"embedding_type"`
+	EmbeddingSpace int    `json:"embedding_space"`
+}
+
+type SpannerSearchConfig struct {
+	SearchAlgorithm SearchAlgorithm `json:"search_algorithm"`
+	EmbeddingConfig EmbeddingConfig `json:"embedding_config"`
+	GraphConfig     GraphConfig     `json:"graph_config"`
+}
