552f69b987892a8c2c80e0d733c7eac8cf735a10
shixiao-coder <shixiao@google.com>
2026-05-05 18:19:18 -0400

    Update the disaster event query by splitting a single match to multiple matches

--- internal/server/spanner/golden/query_builder/get_event_collection_dcids.sql
+++ internal/server/spanner/golden/query_builder/get_event_collection_dcids.sql
@@ -1,8 +1,12 @@
 		@{force_join_order=true}
-		GRAPH DCGraph MATCH (event:Node)-[:Edge {predicate: 'affectedPlace', object_id: 'country/LBR'}]->(), (event)-[:Edge {predicate: 'typeOf', object_id: 'FireEvent'}]->(), (event)-[:Edge {predicate: 'startDate'}]->(dateNode:Node)
+		GRAPH DCGraph
+		MATCH (event:Node)-[:Edge {predicate: 'typeOf', object_id: 'FireEvent'}]->()
+		WITH DISTINCT event
+		MATCH (event:Node)-[:Edge {predicate: 'affectedPlace', object_id: 'country/LBR'}]->()
+		MATCH (event:Node)-[:Edge {predicate: 'startDate'}]->(dateNode:Node)
 		WHERE 
 			SUBSTR(dateNode.value, 1, 7) = '2020-10'
-		MATCH (event)-[magEdge:Edge {predicate: 'area'}]->()
+		MATCH (event:Node)-[magEdge:Edge {predicate: 'area'}]->()
 		RETURN DISTINCT 
 			event.subject_id AS dcid,
 			magEdge.object_id AS magnitude
