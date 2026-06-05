ac7e4c32cea0f05b89da92c05744d5712ec70e1e
Christie Ellks <calinc@google.com>
2026-06-05 13:32:29 -0700

    nit fix test

--- internal/server/handler_v2_test.go
+++ internal/server/handler_v2_test.go
@@ -271,10 +271,10 @@ func TestShouldRouteResolveToDispatcher(t *testing.T) {
 			wantRoute:       false,
 		},
 		{
-			desc:            "Topic resolver with Spanner enabled -> route",
+			desc:            "Topic resolver with Spanner enabled -> don't route (bypassed to local in PR 1)",
 			useSpannerGraph: true,
 			resolver:        resolve.ResolveResolverTopic,
-			wantRoute:       true,
+			wantRoute:       false,
 		},
 		{
 			desc:            "Topic resolver with Spanner disabled -> don't route",
