ed0a6823a51b72d39696b04726e2617d4005fdd0
Carolyn Au <cyin@google.com>
2026-07-28 16:43:24 -0700

    [feature-flags] Replace timezone lock with PR review warning annotations and assign oncaller CODEOWNERS for prod flags

--- deploy/featureflags/deploy_flags.sh
+++ deploy/featureflags/deploy_flags.sh
@@ -39,6 +39,8 @@ set -e
 if [[ -z "$BUILD_ID" ]]; then
   echo "WARNING: This script is not running on Cloud Build."
   echo "Feature flags should only be updated via build trigger after PR review."
+  echo "Please conduct a careful review before restarting production servers & coordinate with the oncaller."
+  echo "Remember to additionally announce this on the oncall thread."
   read -r -p "Proceed with UNSAFE deployment? (y/N) " response
   if [[ ! "$response" =~ ^([yY])$ ]]; then
     echo "Deployment aborted."
