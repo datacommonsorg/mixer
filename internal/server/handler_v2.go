08039a1bda9db99ccd96402bdc02ae94f975c378
Natalie Diaz <nhdiaz@google.com>
2026-08-14 17:24:22 -0700

    address gemini comments

--- internal/server/handler_v2.go
+++ internal/server/handler_v2.go
@@ -562,6 +562,10 @@ func (s *Server) V2BulkVariableInfo(
 	if err != nil {
 		return nil, err
 	}
+	if v2Resp == nil {
+		return &pbv1.BulkVariableInfoResponse{}, nil
+	}
+	v2Resp = proto.Clone(v2Resp).(*pbv1.BulkVariableInfoResponse)
 
 	// The new response will not contain all legacy V1 fields.
 	// To ensure the new response is sufficient, clear all legacy fields until swapping over to the new backend.
@@ -609,11 +613,14 @@ func (s *Server) V2BulkVariableGroupInfo(
 	v2StartTime := time.Now()
 
 	// Fetch and merge local data and remote data, using the v2 API path for remote fetch.
-	v1Resp, err := s.fetchAndMergeBulkVariableGroupInfo(ctx, in, "/v2/bulk/info/variable-group")
+	v2Resp, err := s.fetchAndMergeBulkVariableGroupInfo(ctx, in, "/v2/bulk/info/variable-group")
 	if err != nil {
 		return nil, err
 	}
-	v2Resp := proto.Clone(v1Resp).(*pbv1.BulkVariableGroupInfoResponse)
+	if v2Resp == nil {
+		return &pbv1.BulkVariableGroupInfoResponse{}, nil
+	}
+	v2Resp = proto.Clone(v2Resp).(*pbv1.BulkVariableGroupInfoResponse)
 
 	convertV1ToV2BulkVariableGroupInfo(v2Resp)
 
@@ -655,14 +662,14 @@ func (s *Server) V2GetLocationsRankings(
 ) (*pb.GetLocationsRankingsResponse, error) {
 
 	// Fetch and merge local data and remote data, using the v2 API path for remote fetch.
-	v1Resp, err := s.fetchGetLocationsRankings(ctx, in, "/v2/place/ranking")
+	v2Resp, err := s.fetchGetLocationsRankings(ctx, in, "/v2/place/ranking")
 	if err != nil {
 		return nil, err
 	}
-	if v1Resp == nil {
+	if v2Resp == nil {
 		return &pb.GetLocationsRankingsResponse{}, nil
 	}
-	v2Resp := proto.Clone(v1Resp).(*pb.GetLocationsRankingsResponse)
+	v2Resp = proto.Clone(v2Resp).(*pb.GetLocationsRankingsResponse)
 
 	// Set the Legacy field to true to indicate that this response is from the V1 implementation.
 	v2Resp.Legacy = true
